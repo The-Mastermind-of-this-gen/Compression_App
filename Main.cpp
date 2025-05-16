7105c4a9511a5615ccda3e9a3eefdbd7c04e1115
Mohamed Hazem <nokiabusiness3000@gmail.com>
2025-05-16 18:41:28 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,12 +2,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <new>
-#include "Heap.h"//we made this class
+#include "Heap.h"//We made this class
+#include "HaffmanTree.h"// We also made this class
 
 int main() 
 {
-	Heap<int>* ana = new Heap<int>();
-
-	
+		
 	return 0;
 }
