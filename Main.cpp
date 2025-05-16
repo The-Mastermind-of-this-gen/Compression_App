6fc5f274acfd37e2317b2321c9c4097df21eb3fa
The-Mastermind-of-this-gen <nokiabusiness3000@gmail.com>
2025-05-16 16:59:01 +0300

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include <new>
+#include "Heap.h"//we made this class
+
+int main() 
+{
+	Heap<int>* ana = new Heap<int>();
+
+	
+	return 0;
+}
